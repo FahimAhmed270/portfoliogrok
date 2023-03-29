4ac069b7c2e9352910ed0c54b14ccf196d8663e5
FahimAhmed270 <56820604+FahimAhmed270@users.noreply.github.com>
2023-03-30 01:48:08 +0600

    hi

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from './Component/Header/Header'
+import './App.css'
+
 
 const App = () => {
   return (
     <div>
-      
+      <Header/>
     </div>
   )
 }
