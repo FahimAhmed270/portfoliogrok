6e9b562eb78bb6ce9ee9c4433240fd9fbc4a16b2
FahimAhmed270 <56820604+FahimAhmed270@users.noreply.github.com>
2023-03-30 02:53:01 +0600

    end

--- portfolio/src/Component/Header/Header.jsx
+++ portfolio/src/Component/Header/Header.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import {FaTimes} from 'react-icons/fa'
+import React, { useState } from 'react'
+
 import './Header.css'
 import logo from "../pic/logo.png"
 
 const Header = () => {
+  //fixed header
+
+  window.addEventListener('scroll', function(){
+    const header = this.document.querySelector('.header')
+    header.classList.toggle('active', this.window.screenY > 100)
+  })
+
+  //mobile toggle 
+
+  const [mobile, setMobile] = useState(false)
+
   return (
     <>
       <header className="header">
@@ -12,7 +23,7 @@ const Header = () => {
                 <img src={logo} alt="" />
             </div>
             <div className="navlink">
-                <ul className="link f_flex uppercase">
+              <ul className={mobile ? 'nav-link-mobile' : 'link f_flex uppercase'} onClick={()=> setMobile(false)}>
                     <li><a href="#home">home</a></li>
                     <li><a href="#features">features</a></li>
                     <li><a href="#portfolio">portfolio</a></li>
@@ -21,8 +32,10 @@ const Header = () => {
                     <li><a href="#blog">blog</a></li>
                     <li><a href="#contact">contact</a></li>
                     <li><button className='home-btn'>BUY NOW</button></li>
-                    </ul>
-                    <button className="toggle"><FaTimes/></button>
+                </ul>
+                  <button className="toggle" onClick={()=> setMobile(!mobile)}>
+                    {mobile ? <i className='fa-solid fa-x close home-btn '></i> : <i className='fa-solid fa-bars open'></i>}
+                  </button>
             </div>
           </div>
       </header>
